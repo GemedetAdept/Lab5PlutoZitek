e6a020b284d2de20c19c1f6038b60d7b173fcff7
Vivian Zitek <vivian.zitek@gmail.com>
2022-10-07 22:44:32 -0600

    Add echoEcho function.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// Lab 5 - Functions and Methods
 
+// "Create a function that asks for user input and returns the user input"
+void echoEcho() {
+
+	Console.Write("Enter something to be echoed back > ");
+
+	string userInput = "";
+	userInput = Console.ReadLine();
+
+	Snippet.Break();
+	Console.WriteLine($"You said: \"{userInput}\"");
+	Console.ReadKey();
+}
+
+echoEcho();
